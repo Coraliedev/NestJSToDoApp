ac5666fb1c30b411a82cbfd408a5a6c1a5c3f812
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-10-26 15:46:27 +0200

    feat(type): type todos

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './interface/todo.interface';
 
 @Injectable()
 export class TodosService {
-  todos = [
+  todos: Todo[] = [
     {
       id: 1,
       title: 'Todo 1',
       description: 'Description 1',
+      done: false,
     },
     {
       id: 2,
       title: 'Todo 2',
       description: 'Description 2',
+      done: true,
+    },
+    {
+      id: 3,
+      title: 'Todo 3',
+      description: 'Description 3',
+      done: false,
     },
   ];
 
-  findAll(): any[] {
+  findAll(): Todo[] {
     return this.todos;
   }
 }
